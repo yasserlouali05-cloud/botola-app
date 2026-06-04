d08b672c0585523d72294fdd56865fd3b7d3ddac
Yasser <yasserlouali05@gmail.com>
2026-06-04 03:07:35 +0100

    fix homepage

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.get("/teams", async (req, res) => {
     try {
-
         const response = await axios.get(
             "https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=UEFA%20Champions%20League"
         );
@@ -16,15 +22,14 @@ app.get("/teams", async (req, res) => {
         res.json(response.data);
 
     } catch (error) {
-
-        console.log(error.message);
-
         res.status(500).json({
             error: error.message
         });
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
